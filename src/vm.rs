30f98ab8f8f2a63a99cbe2ee96b16dd985b8448f
Yhya <yhyamobile57@gmail.com>
2024-02-28 13:13:59 +0200

    add `require` function to the globals

--- src/vm.rs
+++ src/vm.rs
@@ -4,8 +4,12 @@ use crate::os::OsModule;
 use crate::path::PathModule;
 use crate::process::ProcessModule;
 
+use rquickjs::function::Func;
 use rquickjs::loader::{BuiltinResolver, FileResolver, ModuleLoader, ScriptLoader};
-use rquickjs::{AsyncContext, AsyncRuntime, CatchResultExt, CaughtError, Ctx, Module, Object};
+use rquickjs::{
+    AsyncContext, AsyncRuntime, CatchResultExt, CaughtError, Ctx, Module, Object,
+    Result as QuickJsResult, Value,
+};
 
 use std::path::Path;
 use std::process::exit;
@@ -58,13 +62,13 @@ impl VirtualMachine {
                 .with_extension("cjs"),
         );
 
-        let runtime = AsyncRuntime::new().expect("failed to create an AsyncRuntime");
+        let runtime = AsyncRuntime::new().expect("Could not create an AsyncRuntime");
 
         runtime.set_loader(resolver, loader).await;
 
         let context = AsyncContext::full(&runtime)
             .await
-            .expect("failed to create an AsyncContext");
+            .expect("Could not create an AsyncContext");
 
         VirtualMachine { context, runtime }
     }
@@ -72,9 +76,23 @@ impl VirtualMachine {
     pub async fn init(&self) {
         self.context
             .with(|ctx| {
+                let globals = ctx.globals();
+
+                globals
+                    .set(
+                        "require",
+                        Func::from(|ctx, specifier: String| -> QuickJsResult<Value> {
+                            let module = Module::import::<Object, _>(&ctx, specifier)?;
+
+                            Ok(module.get("default").unwrap_or(module.into_value()))
+                        }),
+                    )
+                    .catch(&ctx)
+                    .unwrap_or_else(|err| VirtualMachine::print_error_and_exit(ctx.clone(), err));
+
                 crate::console::init(&ctx)
                     .catch(&ctx)
-                    .unwrap_or_else(|err| VirtualMachine::print_error_and_exit(ctx, err));
+                    .unwrap_or_else(|err| VirtualMachine::print_error_and_exit(ctx.clone(), err));
             })
             .await
     }
